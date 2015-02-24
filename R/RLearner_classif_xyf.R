d468db3064dfad023af48ac6663208b89a709473
CC <christinacyris@web.de>
2015-02-24 14:30:08 +0100

    bla

--- R/RLearner_classif_xyf.R
+++ R/RLearner_classif_xyf.R
@@ -18,7 +18,7 @@ makeRLearner.classif.xyf = function() {
     properties = c("numerics", "twoclass", "multiclass", "prob"),
     name = "X-Y fused self-organising maps",
     short.name = "xyf",
-    note = ""
+    note = "Voll blöd!"
   )
 }
 
